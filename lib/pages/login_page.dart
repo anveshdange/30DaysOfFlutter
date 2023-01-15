5344b1766c5f93304217e77cad8cdf6882fca406
anveshdange <anveshdange19@gmail.com>
2023-01-15 11:39:36 +0530

    succesfully completed day 6 of the flutter code

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,8 +2,17 @@
 import 'package:codepur/utils/routes.dart';
 import 'package:flutter/material.dart' ;
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  String name = ""; // this is the name we want to print in the app welcome bar
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,9 +29,9 @@ class LoginPage extends StatelessWidget {
             const SizedBox(
               height:20.00,
             ),
-            const Text(
-                'Welcome',
-              style: TextStyle(
+            Text(
+                'Welcome $name',
+              style: const TextStyle(
                 fontSize: 28,
                 fontWeight: FontWeight.bold,
               ),
@@ -38,6 +47,10 @@ class LoginPage extends StatelessWidget {
                       hintText: "Enter Username",
                       labelText: "Username",
                     ),
+                    onChanged: (value){
+                      name = value;
+                      setState(() {}); // this calls the build method again
+                    },
                   ),
 
                   TextFormField(
@@ -50,16 +63,50 @@ class LoginPage extends StatelessWidget {
 
                   const SizedBox(height:40.00),
 
-                  ElevatedButton(
-                    style: TextButton.styleFrom(
-                      minimumSize: const Size(150,40),
-                    ),
-                    onPressed: () {
-                      // we will use the navigator class to switch from login page to the home page
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true; // this will change the boolean value
+                      });
+                      await Future.delayed(const Duration(seconds: 1));
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
-                    child: const Text("Login"),
+                    child: AnimatedContainer(
+                      duration:const Duration(seconds: 1),
+                      width: changeButton ? 50 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        //shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(changeButton ? 50.00 : 8.00),
+                      ),
+                      child: changeButton
+                          ? const Icon(
+                          Icons.done,
+                          color: Colors.white,
+                      )
+                          : const Text(
+                          'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
                   ),
+
+                  // ElevatedButton(
+                  //   style: TextButton.styleFrom(
+                  //     minimumSize: const Size(150,40),
+                  //   ),
+                  //   onPressed: () {
+                  //     // we will use the navigator class to switch from login page to the home page
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  //   child: const Text("Login"),
+                  // ),
                 ],
               ),
             ),
